7b2131c74eeab5d5fc9b6ed38cfad17e1d89542b
Hoon <yimsh4507@gmail.com>
2022-11-18 17:22:59 -0500

    Updated login.js - cannot submit error fixed

--- src/pages/login.js
+++ src/pages/login.js
@@ -12,29 +12,30 @@ export default function Login() {
         <Card style={{ width: "25rem", padding: "30px 20px" }}>
             <Stack gap={3}>
                 <Card.Title style={{ "margin-bottom": "20px" }}>Log In</Card.Title>
+                <form>
+                    <Form.Group>
+                        <Form.Label htmlFor="email_input">Email</Form.Label>
+                        <Form.Control
+                            id="email_input"
+                            type="email"
+                            placeholder="abc123@gmail.com" />
+                    </Form.Group>
 
-                <Form.Group>
-                    <Form.Label htmlFor="email_input">Email</Form.Label>
-                    <Form.Control
-                        id="email_input"
-                        type="email"
-                        placeholder="abc123@gmail.com" />
-                </Form.Group>
-
-                <Form.Group>
-                    <Form.Label htmlFor="password_input">Password</Form.Label>
-                    <Form.Control
-                        id="password_input"
-                        type="password" />
-                </Form.Group>
-                <Row className="mt-3">
-                    <Col>
-                        <Button href="signup" variant="secondary" style={{ width: "100%" }}>Sign Up</Button>
-                    </Col>
-                    <Col>
-                        <Button variant="success" type="submit" style={{ width: "100%" }}>Log In</Button>
-                    </Col>
-                </Row>
+                    <Form.Group>
+                        <Form.Label htmlFor="password_input">Password</Form.Label>
+                        <Form.Control
+                            id="password_input"
+                            type="password" />
+                    </Form.Group>
+                    <Row className="mt-3">
+                        <Col>
+                            <Button href="signup" variant="secondary" style={{ width: "100%" }}>Sign Up</Button>
+                        </Col>
+                        <Col>
+                            <Button variant="success" type="submit" style={{ width: "100%" }}>Log In</Button>
+                        </Col>
+                    </Row>
+                </form>
             </Stack>
         </Card >
     );
