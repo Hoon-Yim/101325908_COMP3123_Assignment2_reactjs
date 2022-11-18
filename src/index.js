3104d2574af297cf6cb8cceebdc0c876e3205a9b
Hoon <yimsh4507@gmail.com>
2022-11-18 15:54:36 -0500

    Updated index.js - /login Router added

--- src/index.js
+++ src/index.js
@@ -2,17 +2,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// css
+// Pages
+import Login from "./pages/login";
+
+// CSS
 import './index.css';
 
 // Components
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <div>
-            <Header />
-        </div>
+        <Header />
+        <BrowserRouter>
+            <Routes>
+                <Route path="login" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 );
