08811fe2a17e60f13576d3b5e240538ab686c2b3
Hoon <yimsh4507@gmail.com>
2022-11-18 15:40:06 -0500

    Added login.js - login page

---
+++ src/pages/login.js
@@ -0,0 +1,18 @@
+// Modules
+import React from "react";
+
+// CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Components
+import { Container } from "react-bootstrap";
+
+export default function Login() {
+    return (
+        <Container>
+            <div>
+                asdaf
+            </div>
+        </Container>
+    );
+}
